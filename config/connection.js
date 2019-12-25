cef0b036a99c8776c6b0963babdb619f33d05407
Dean McCluskey <dean1250@gmail.com>
2019-12-24 16:41:20 -0800

    update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
 // connection.js 
 
-// Set up MySQL connection
+// Set up MySQL for connection
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "dmc2SQL!",
-  database: "burgers_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "dmc2SQL!",
+    database: "burgers_db"
 });
 
 // Make connection
 connection.connect((err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for ORM to use
