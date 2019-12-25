81d63b5e165e2c498b056a6970c4d09f0ee11c76
Dean McCluskey <dean1250@gmail.com>
2019-12-24 17:47:32 -0800

    update connection.js pw

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "dmc2SQL!",
+    password: "dmc2SQL!" || "",
     database: "burgers_db"
 });
 
