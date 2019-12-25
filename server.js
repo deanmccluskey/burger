2bdc7601822ee1e8d9934a2e68037b44e89b6333
Dean McCluskey <dean1250@gmail.com>
2019-12-24 18:19:37 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,83 +1,32 @@
-// burger -- HW14 - Node Express Handlebars App
+// server.js
+// Burger App -- HW14 - Node/Express/Handlebars App
+// Copyright 2019 Dean McCluskey
 
-
-// Required packages
+// Set up Express for application
 var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
-// Create Express app
+// Set up port of app, process.env.PORT lets port be set by Heroku
+var PORT = process.env.PORT || 3000;
 var app = express();
 
-// Set port of application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for app from "public" directory in the app directory
+app.use(express.static("public"));
 
-// Set up Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+// Parse request body as JSON
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// Set up Handlebars engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// Set up Express Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// Configuire DB connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "dmc2SQL!",
-    database: "burgers_db"
-});
+// Import routes and give server access to them
+var routes = require("./controllers/burger_controller.js");
+app.use(routes);
 
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-// Root get route.
-app.get("/", (req, res) => {
-    connection.query("SELECT * FROM burgers;", (err, data) => {
-        if (err) {
-            throw err;
-        }
-        // Test it.
-        // console.log('The solution is: ', data);
-        // Test it.
-        // res.send(data);
-        res.render("index", { burgers: data });
-    });
-});
-
-
-
-
-
-
-
-
-// Post route -> back to home
-app.post("/", (req, res) => {
-    // Test it.
-    // console.log('You sent, ' + req.body.wish);
-
-    // Test it.
-    // res.send('You sent, ' + req.body.wish)
-
-    connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burger],
-        (err, result) => {
-            if (err) {
-                throw err;
-            }
-            res.redirect("/");
-        });
-});
-
-// Start our server so that it can begin listening to client requests.
+// Start server to listen to client requests
 app.listen(PORT, () => {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
-});
+  });
